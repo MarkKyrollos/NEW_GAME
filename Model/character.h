dc75df3f018b4d5812cb6e2efd291643e554a0f7
mahmoud <mahmoud9304@aucegypt.edu>
2022-04-20 17:33:22 +0200

    Revert "character can now move everywhere"
    This reverts commit 7e2360e2e9c50059d93437c8f7299811af1f8dd5.

--- Model/character.h
+++ Model/character.h
@@ -2,46 +2,39 @@
 #define _CHARACTER_H
 
 #include<QObject>
+#include<QKeyEvent>  // for utilizing key presses
+#include <QGraphicsPixmapItem> // for using the QPixmap class that would map our character onto the screen
 #include <QMainWindow>
-#include <QGraphicsPixmapItem>
-#include <QKeyEvent>
-/*#include "projectile.h"*/
+#include "projectile.h"
 
-enum direct {up=1, right, left, down};
 
 class character : public QObject //, public QGraphicsPixmapItem (include this class in inherited classes like player and enemy since each one would have its own jpeg)
 {
     Q_OBJECT
 public:
     int health;
-    /*QString name;*/
+    QString name;
     float movement_speed;
     bool is_alive; // if character exists (may die)
-    direct face;
+    int face;
     int row;
     int col;
-    QVector<QVector<int>>* map;
-    QVector<QVector<QVector<character*>>>* charLoc;
-    bool Player;
+
 
     // create an integer 2d array that will store the map text file (which would be converted to int in main) according to different maps in different rooms
     // then later on in derived classes we will decide the starting position of enemy/player using setPixMap using this
     // also make sure tha tthe constructors of derived classes would take an integer 2D array in its parameter (in an initializer list fashion?)
 
 
-    character(int helth, float mvmt_spd, bool alive, direct Facer, int Rowd, int Cold, QVector<QVector<int>> &map, QVector<QVector<QVector<character*>>> &charLoc, bool Playa);
+    character(int helth, QString NAME, float mvmt_spd, bool alive, int Facer, int Rowd, int Cold);
 
     void shoot(); // i set this shooting function to be a pure virtual one since player shots are manually done using spacebar while enemy shots are random
 
 
-
 public slots:   // added this, i can push everything onto github once im done instead of modifying each step i make
-    void keyPressEvent(QKeyEvent* event);
+    virtual void keyPressEvent(QKeyEvent* event);
 
-    void moveUp(QKeyEvent* event, QVector<QVector<QVector<character*>>> &charLoc);
-    void moveDown(QKeyEvent* event, QVector<QVector<QVector<character*>>> &charLoc);
-    void moveRight(QKeyEvent* event, QVector<QVector<QVector<character*>>> &charLoc);
-    void moveLeft(QKeyEvent* event, QVector<QVector<QVector<character*>>> &charLoc);
+    // add another function for keyboard movement
 
 };
 
