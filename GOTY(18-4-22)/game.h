43f6f104a8d10dd99dda74b7bbab0dfd4141005a
MagdElDinAhmed <magdeldin@aucegypt.edu>
2022-05-13 20:44:36 +0200

    Fixed main menu bug

--- GOTY(18-4-22)/game.h
+++ GOTY(18-4-22)/game.h
@@ -1,7 +1,23 @@
 #ifndef GAME_H
 #define GAME_H
-
-
+#include "mainwindow.h"
+#include <QApplication>
+#include <QGraphicsView> //sets window title and set fixed width
+#include <QGraphicsScene>
+#include <QBrush>
+#include <QFile>
+#include <QTextStream>
+#include <QGraphicsPixmapItem>
+#include <QKeyEvent>
+#include "player.h"
+#include "character.h"
+#include "projectile.h"
+#include "enemy.h"
+#include "major.h"
+#include "minor.h"
+#include "boss.h"
+#include <QVector>
+#include "doortimer.h"
 class game
 {
 public:
